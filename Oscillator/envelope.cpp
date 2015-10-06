f44a20e20b5e362495ba5465e7585d8744b44c87
Andreas Plaß <andreas.plass@haw-hamburg.de>
2015-10-06 15:14:48 +0200

    Envelope: Attack

--- Oscillator/envelope.cpp
+++ Oscillator/envelope.cpp
@@ -3,14 +3,19 @@
 
 Envelope::Envelope()
     : sampleRate(0), gain(0), state(OFF), releaseSeconds(0)
+    , attackSeconds(0)
 {
     setReleaseSeconds(0.5);
+    setAttackSeconds(1);
 }
 void Envelope::setSampleRate(float sampleRate){
     this->sampleRate = sampleRate;
 }
 void Envelope::setReleaseSeconds(float seconds){
     this->releaseSeconds = seconds;
+}
+void Envelope::setAttackSeconds(float seconds){
+    this->attackSeconds = seconds;
 }
 
 void Envelope::setState(State state){
@@ -27,6 +32,13 @@ void Envelope::setState(State state){
 }
 
 float Envelope::process(float input){
+    if(state == ATTACK){
+        gain += 1/(sampleRate * attackSeconds);
+        if (gain >= 1){
+            setState(ON);
+        }
+    }
+
     if(state == RELEASE){
         gain -= 1/(sampleRate * releaseSeconds);
         if (gain <= 0){
